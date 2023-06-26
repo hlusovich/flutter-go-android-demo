e191aed5d8478f7d6520501faa285053d1c1806d
hlusovich <bigklu95@gmail.com>
2023-06-26 11:47:20 +0300

    add platform channels for macos

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,12 +1,33 @@
 import Cocoa
 import FlutterMacOS
+import IOKit.ps
+
 
 class MainFlutterWindow: NSWindow {
-  override func awakeFromNib() {
-    let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
+   override func awakeFromNib() {
     self.setFrame(windowFrame, display: true)
+  
+    let channel = FlutterMethodChannel(
+      name: "example.com/gomobileNative",
+      binaryMessenger: flutterViewController.engine.binaryMessenger)
+    channel.setMethodCallHandler { (call, result) in
+      if call.method == "getBirdsList"  {
+      result("instance.getBirds()")
+   }
+
+   else if call.method == "getRandomNumber"  {
+      result("instance.randomNumber()")
+   }
+
+   else if call.method == "getBirdInfo"  {
+      let args = call.arguments as! Dictionary<String, String>
+      let isDebug = args["bird"]
+      result("instance.getBirdInfo(isDebug)")
+  }
+   else {
+      result(FlutterMethodNotImplemented)
+   }
+    }
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
