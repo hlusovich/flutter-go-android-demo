270542b0d6c9ab5f779745c24f97e9a9a3c95a1f
Alina Kulikova <akulikava@datamola.com>
2023-06-26 14:23:59 +0300

    add GomobileNative for macos

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,28 +1,33 @@
 import Cocoa
 import FlutterMacOS
 import IOKit.ps
+import GomobileNative
 
 
 class MainFlutterWindow: NSWindow {
    override func awakeFromNib() {
+          let flutterViewController = FlutterViewController()
+    let windowFrame = self.frame
+    self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
   
     let channel = FlutterMethodChannel(
       name: "example.com/gomobileNative",
       binaryMessenger: flutterViewController.engine.binaryMessenger)
     channel.setMethodCallHandler { (call, result) in
+    let instance = GomobileNativeGoMobileLib()
       if call.method == "getBirdsList"  {
-      result("instance.getBirds()")
+      result(instance.getBirds())
    }
 
    else if call.method == "getRandomNumber"  {
-      result("instance.randomNumber()")
+      result(instance.randomNumber())
    }
 
    else if call.method == "getBirdInfo"  {
       let args = call.arguments as! Dictionary<String, String>
       let isDebug = args["bird"]
-      result("instance.getBirdInfo(isDebug)")
+      result(instance.getBirdInfo(isDebug))
   }
    else {
       result(FlutterMethodNotImplemented)
