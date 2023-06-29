1082b8ce8e6645b08f4e26536c9808987d415474
hlusovich <mklusovich@datamola.com>
2023-06-29 16:37:00 +0300

    add implementation for linux methods

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -26,7 +26,7 @@ static void method_call_cb(FlMethodChannel *channel,
   {
             auto value = {91, 34, 83, 112, 97, 114, 114, 111, 119, 34, 44, 34, 80, 105, 103, 101, 111, 110, 34, 44, 34, 72, 101, 114, 111, 110, 34, 93}; 
 
-  FlValue *res = fl_value_new_array(value);
+  FlValue *res = fl_value_new_list(value);
 
   g_autoptr(FlMethodResponse) response = FL_METHOD_RESPONSE(fl_method_success_response_new(res));
   g_autoptr(GError) error = nullptr;
