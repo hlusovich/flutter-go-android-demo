e05bbc8506a26f95cc2d67365553718aaa08dca6
hlusovich <mklusovich@datamola.com>
2023-06-29 14:26:28 +0300

    change linux platform channels

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -20,7 +20,7 @@ static void battery_method_call_handler(FlMethodChannel* channel,
                                         gpointer user_data) {
   g_autoptr(FlMethodResponse) response = nullptr;
   if (strcmp(fl_method_call_get_name(method_call), "getBirdsList") == 0) {
-    response = get_battery_level();
+    
   } else {
     response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
   }
