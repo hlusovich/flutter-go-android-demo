8421405f77562360a533f53a3d9cacb4c5f1139f
hlusovich <mklusovich@datamola.com>
2023-06-29 16:08:55 +0300

    add new package for linux build

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -21,8 +21,10 @@ static void method_call_cb(FlMethodChannel *channel,
   const gchar *method = fl_method_call_get_name(method_call);
   if (strcmp(method, "getBirdsList") == 0)
   {
- FlValue *res = fl_value_new_string("Response from Linux");
- FL_METHOD_RESPONSE(fl_method_success_response_new(res));
+
+auto value = {91, 34, 83, 112, 97, 114, 114, 111, 119, 34, 44, 34, 80, 105, 103, 101, 111, 110, 34, 44, 34, 72, 101, 114, 111, 110, 34, 93}; 
+
+ FL_METHOD_RESPONSE(fl_method_success_response_new(value));
     
 
   }
