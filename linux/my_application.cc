3064d8ce5ee6732981c0fcfa4ae5555ce8325e82
hlusovich <mklusovich@datamola.com>
2023-06-29 17:46:20 +0300

    add implementation for linux methods

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -68,9 +68,8 @@ static void method_call_cb(FlMethodChannel *channel,
   }
   if (strcmp(method, "getBirdInfo") == 0){
   FlValue* args = fl_method_call_get_args(method_call);
-  FlValue(gchar*) *bird = fl_value_lookup_string(args, "bird");
 
-  if (strcmp(method, "Sparrow")) {
+  
   g_autoptr(FlValue) res = fl_value_new_list();
   fl_value_append_take(res, fl_value_new_int(91));
   fl_value_append_take(res, fl_value_new_int(34));
@@ -103,11 +102,7 @@ static void method_call_cb(FlMethodChannel *channel,
   g_autoptr(FlMethodResponse) response = FL_METHOD_RESPONSE(fl_method_success_response_new(res));
   g_autoptr(GError) error = nullptr;
   fl_method_call_respond(method_call, response, &error); 
-  }
-
-  g_autoptr(FlMethodResponse) response = FL_METHOD_RESPONSE(fl_method_success_response_new(res));
-  g_autoptr(GError) error = nullptr;
-  fl_method_call_respond(method_call, response, &error); 
+  
   }
 }
 // Implements GApplication::activate.
